8410f48dff68cd0a95c8b7ed485d433493a7bdf6
ojasuno <ojasuno21@gmail.com>
2022-05-24 14:40:11 +1000

    add IpfsDownload.js

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,58 @@
 import React from "react";
+import { PublicKey } from "@solana/web3.js";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 import HeadComponent from '../components/Head';
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const BY_TWITTER_HANDLE = "ojasuno";
+const BY_TWITTER_LINK = `https://twitter.com/${BY_TWITTER_HANDLE}`;
+const ON_TWITTER_HANDLE = "_buildspace";
+const ON_TWITTER_LINK = `https://twitter.com/${ON_TWITTER_HANDLE}`;
+
 
 const App = () => {
-  
+  // This will fetch the users' publick key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
   
   return (
     <div className="App">
-      <HeadComponent/>
+      {/* <HeadComponent/> */}
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header"> 👾 Konnichiwa 🐉</p>
+          <p className="sub-text">... this store accepts web3 currency!</p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {/* We only render the connect button if publickey doesn't exist */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
+          
+          {/* <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" /> */}
+
         </main>
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
           <a
             className="footer-text"
-            href={TWITTER_LINK}
+            href={BY_TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${BY_TWITTER_HANDLE}`}</a>
         </div>
+
       </div>
     </div>
   );
